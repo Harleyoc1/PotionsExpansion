2c1c673c675db4bde9d8c01a5f71b0cb3a80f77b
Harleyoc1 <harley.oconnor@icloud.com>
2020-10-26 16:13:13 +0000

    Initial commit.

---
+++ src/main/java/com/harleyoconnor/potionsexpansion/ModConstants.java
@@ -0,0 +1,12 @@
+package com.harleyoconnor.potionsexpansion;
+
+/**
+ * @author Harley O'Connor
+ */
+public final class ModConstants {
+
+    public static final String MOD_NAME = "Potions Expansion";
+    public static final String MOD_ID = "potionsexpansion";
+    public static final String MOD_VERSION = "1.16.3-1.0.0";
+
+}
---
+++ src/main/java/com/harleyoconnor/potionsexpansion/PotionsExpansion.java
@@ -0,0 +1,10 @@
+package com.harleyoconnor.potionsexpansion;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author Harley O'Connor
+ */
+@Mod(ModConstants.MOD_ID)
+public final class PotionsExpansion {
+}
