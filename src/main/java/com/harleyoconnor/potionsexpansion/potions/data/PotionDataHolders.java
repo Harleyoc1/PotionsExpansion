f47f24d9c3bd3710bcd6979ab02e9d478c66138c
Harleyoc1 <harley.oconnor@icloud.com>
2020-12-13 18:46:31 +0000

    Added some hard-coded PotionData objects.

--- src/main/java/com/harleyoconnor/potionsexpansion/potions/data/PotionDataHolders.java
+++ src/main/java/com/harleyoconnor/potionsexpansion/potions/data/PotionDataHolders.java
@@ -1,7 +1,9 @@
 package com.harleyoconnor.potionsexpansion.potions.data;
 
+import net.minecraft.potion.Effect;
 import net.minecraft.potion.Effects;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.Potions;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.HashMap;
@@ -15,7 +17,18 @@ public final class PotionDataHolders {
     private static final Map<Potion, PotionData> potionData = new HashMap<>();
 
     public static void initHolders () {
-        ForgeRegistries.POTION_TYPES.forEach(potion -> potionData.put(potion, new PotionData((byte) 1, Effects.HUNGER)));
+        ForgeRegistries.POTION_TYPES.forEach(potion -> {
+            if (Potions.STRENGTH.equals(potion) || Potions.STRONG_STRENGTH.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.WEAKNESS));
+            else if (Potions.WEAKNESS.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.STRENGTH));
+            else if (Potions.HARMING.equals(potion) || Potions.STRONG_HARMING.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.INSTANT_HEALTH));
+            else if (Potions.HEALING.equals(potion) || Potions.STRONG_HEALING.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.INSTANT_DAMAGE));
+            else if (Potions.SWIFTNESS.equals(potion) || Potions.STRONG_SWIFTNESS.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.SLOWNESS));
+            else if (Potions.SLOWNESS.equals(potion) || Potions.STRONG_SLOWNESS.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.SPEED));
+            else if (Potions.POISON.equals(potion) || Potions.STRONG_POISON.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.REGENERATION));
+            else if (Potions.REGENERATION.equals(potion) || Potions.STRONG_REGENERATION.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.POISON));
+            else if (Potions.NIGHT_VISION.equals(potion)) potionData.put(potion, new PotionData((byte) 2, Effects.BLINDNESS));
+            else potionData.put(potion, new PotionData((byte) 1, Effects.HUNGER));
+        });
     }
 
     public static PotionData getPotionData (final Potion potion) {
