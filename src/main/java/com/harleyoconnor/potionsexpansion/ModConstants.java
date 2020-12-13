a3153edd753c1bc7718d5532636f7309de54b56e
Harleyoc1 <harley.oconnor@icloud.com>
2020-12-13 15:02:02 +0000

    Made tooltip localisations and colour codes (subject to change).

--- src/main/java/com/harleyoconnor/potionsexpansion/ModConstants.java
+++ src/main/java/com/harleyoconnor/potionsexpansion/ModConstants.java
@@ -9,4 +9,7 @@ public final class ModConstants {
     public static final String MOD_ID = "potionsexpansion";
     public static final String MOD_VERSION = "1.16.3-1.0.0";
 
+    public static final String TOOLTIP_LOCALISATION = "tooltip.";
+    public static final String POTION_TOOLTIP_LOCALISATION = TOOLTIP_LOCALISATION + "potion.";
+
 }
--- src/main/java/com/harleyoconnor/potionsexpansion/events/TooltipEvent.java
+++ src/main/java/com/harleyoconnor/potionsexpansion/events/TooltipEvent.java
@@ -1,11 +1,12 @@
 package com.harleyoconnor.potionsexpansion.events;
 
+import com.harleyoconnor.potionsexpansion.ModConstants;
 import com.harleyoconnor.potionsexpansion.potions.data.PotionData;
 import com.harleyoconnor.potionsexpansion.potions.data.PotionDataHolders;
 import net.minecraft.item.PotionItem;
-import net.minecraft.potion.Potion;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -20,11 +21,15 @@ public final class TooltipEvent {
         if (!(event.getItemStack().getItem() instanceof PotionItem)) return;
 
         final String potionResourceString = event.getItemStack().getOrCreateTag().getString("Potion");
-        final Potion potion = ForgeRegistries.POTION_TYPES.getValue(new ResourceLocation(potionResourceString.substring(0, potionResourceString.indexOf(':')), potionResourceString.substring(potionResourceString.indexOf(':') + 1)));
-        final PotionData potionData = PotionDataHolders.getPotionData(potion);
+        final PotionData potionData = PotionDataHolders.getPotionData(
+                ForgeRegistries.POTION_TYPES.getValue(new ResourceLocation(
+                        potionResourceString.substring(0, potionResourceString.indexOf(':')),
+                        potionResourceString.substring(potionResourceString.indexOf(':') + 1)
+                )));
 
-        event.getToolTip().add(new StringTextComponent("Difficulty: " + potionData.getDifficulty()));
-        event.getToolTip().add(new StringTextComponent("Opposite Effect: " + potionData.getOppositeEffect().getName()));
+        event.getToolTip().add(new StringTextComponent(""));
+        event.getToolTip().add(new TranslationTextComponent(ModConstants.POTION_TOOLTIP_LOCALISATION + "difficulty", "§b" + potionData.getDifficulty()));
+        event.getToolTip().add(new TranslationTextComponent(ModConstants.POTION_TOOLTIP_LOCALISATION + "opposite", new StringTextComponent("§4" + new TranslationTextComponent(potionData.getOppositeEffect().getName()).getString())));
     }
 
 }
